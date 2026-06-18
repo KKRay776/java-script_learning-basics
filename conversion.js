5c55491c8ba58b5f22b6ab50a1f629c6398b6337
KKRay776 <thedeeeproot@gmail.com>
2026-06-18 22:05:09 +0530

    Add files via upload

---
+++ conversion.js
@@ -0,0 +1,41 @@
+// let score=99
+// console.log(score)
+// console.log(typeof score)
+
+//====================if string converted to number================
+
+
+
+// let score="99"
+// console.log(score)
+// console.log(typeof score)
+
+
+// <-----NOTE:during conversion the first letter of that data type is converted to capital letter and rest of the letters are in small letter
+// let score="99"
+// let numberscore=Number(score)
+// console.log(numberscore)
+// console.log(typeof numberscore)
+
+
+
+
+// let score="99abc"
+// let numberscore=Number(score)
+// console.log(numberscore) // op=Nan(not a number)
+// console.log(typeof numberscore) // (op=number)
+
+// NOTE: if we try to convert a string which has some non-numeric characters then the output will be NaN and the data type of that output will be number.
+
+//<===================null=> nmuber=================>
+
+
+    // let score=null
+    // let numberscore=Number(score)
+    // console.log(numberscore) // op=0
+    // console.log(typeof numberscore) // op=number
+
+     let score=null
+     let numberscore=Number(score)
+     console.log(numberscore) // op=0
+        console.log(typeof numberscore) // op=number
