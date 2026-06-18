3fa88543080ac6340471bd7ba09a44fbf6a018a2
KKRay776 <thedeeeproot@gmail.com>
2026-06-18 21:36:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("hii")
+console.log("hello")
+accountEmail="kkroy@gmail.com"
+accountpassword="123"
+accountcity="bhagalpur"
+console.table([accountEmail,accountpassword,accountcity])
+let accountstate;
+console.log(accountstate)
