01cb126dea2ef500187996b81ed5c42d29adb27a
KKRay776 <thedeeeproot@gmail.com>
2026-06-19 08:57:54 +0530

    Uncomment conversion examples in 02_conversion.js

--- 02_conversion.js
+++ 02_conversion.js
@@ -27,6 +27,14 @@
 
 // NOTE: if we try to convert a string which has some non-numeric characters then the output will be NaN and the data type of that output will be number.
 
+
+
+
+//NOTE: NaN is a special numeric value that represents an invalid or failed number conversion, which is why its data type remains 'number'.
+
+
+
+
 //<===================null=> nmuber=================>
 
 
@@ -35,7 +43,44 @@
     // console.log(numberscore) // op=0
     // console.log(typeof numberscore) // op=number
 
-     let score=null
-     let numberscore=Number(score)
-     console.log(numberscore) // op=0
-        console.log(typeof numberscore) // op=number
+    //  let score=null
+    //  let numberscore=Number(score)
+    //  console.log(numberscore) // op=0
+    //     console.log(typeof numberscore) // op=number
+
+    //NOTE: if we try to convert null into number then the output will be 0 and the data type of that output will be number.
+
+    //AND THIS THING IS SAME FOR UNDEFINED ALSO
+
+    // let score=undefined
+    // let numberscore=Number(score)
+    // console.log(numberscore) // op=NaN
+    // console.log(typeof numberscore) // op=number 
+
+
+ //NOTE: if we try to convert undefined into number then the output will be NaN and the data type of that output will be number.
+
+
+ //<===================boolean=> number=================>
+
+    // let score=true
+    // let numberscore=Number(score)
+    // console.log(numberscore) // op=1
+    // console.log(typeof numberscore) // op=number
+
+    // TRUE=1; FALSE=0
+
+
+
+
+    // <===================string=> boolean=================>
+
+    // let isloggedin="true"
+    // let booleanisloggedin=Boolean(isloggedin)
+    // console.log(booleanisloggedin) // op=true
+    // console.log(typeof booleanisloggedin) // op=boolean
+
+    // NOTE: if we try to convert a non-empty string into boolean then the output will be true and the data type of that output will be boolean.
+    // and empty string will be false
+
+
